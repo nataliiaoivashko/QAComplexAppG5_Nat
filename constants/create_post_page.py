5c86a6d29b8aeb5dad5b3a3ece31b82ad2e5efc1
nataliiaoivashko <nataliiaoivashko@gmail.com>
2022-10-04 13:43:28 -0400

    4-October-fixing my test (create full post) at home, now it works. but it is usable only with hardcoede values for dropdown and for checkbox. I have to re-do it so that it is usable with any chosen value.

--- constants/create_post_page.py
+++ constants/create_post_page.py
@@ -1,25 +1,19 @@
 class CreatePostPageConsts:
     TITLE_FIELD_XPATH = ".//input[@id='post-title']"
     BODY_FIELD_XPATH = ".//textarea[@id='post-body']"
-
+    CREATE_POST_BUTTON_XPATH = ".//button[@class='btn btn-primary']"
     SUCCESS_MESSAGE_XPATH = ".//div[@class='alert alert-success text-center']"
     SUCCESS_MESSAGE_TEXT = "New post successfully created."
-
-    CREATE_POST_BUTTON_XPATH = ".//button[@class='btn btn-primary']"
-
-    CHECKBOX_INPUT_XPATH = ".//input[contains(@type,'checkbox')]"
-
-    DROPDOWN_LIST_XPATH = ".//select[@name='select1']"
-
-    OPTION_ALL_USERS = "All Users"
-    OPTION_ONE_PERSON = "One Person"
-    OPTION_GROUP_MESSAGE = "Group Message"
-    VISIBILITY_SELECTION_XPATH = ".//option[@value='{option}']"
-
-    SELECT_XPATH_GROUP = './/i'
-    SELECT_XPATH_TEXT = "One Person"
-    CHECKBOX_UNIQUE_YES = ".//p[contains(.,'Is this post unique? : yes')]"
-    CHECKBOX_TEXT = "Is this post unique?: yes"
-
-    # CHECKBOX_UNIQUE_NO = ".//p[contains(.,'Is this post unique? : no')]"
-    # SELECT_XPATH_GROUP = ".//u[contains(.,'Group Message')]"
+    CREATED_TITLE_XPATH = ".//h2"
+    CREATED_BODY_CONTENT_XPATH = ".//div[@class='body-content']/p[contains(text(), '{body}')]"
+    CHECKBOX_UNIQUE_POST_INPUT_XPATH = ".//input[contains(@type,'checkbox') and (@name='uniquePost')]"
+    DROPDOWN_LIST_XPATH = ".//select[@name='select1']"  # click on the dropdown list
+    VISIBILITY_SELECTION_XPATH = ".//option[@value='{option}']"  # choosing the option value from dropdown
+    OPTION_ALL_USERS_TEXT = "All Users"  # dropdown option 1
+    OPTION_ONE_PERSON_TEXT = "One Person"  # dropdown option 2
+    OPTION_GROUP_MESSAGE_TEXT = "Group Message"  # dropdown option 3
+    SELECTED_VISIBILITY_VALUE_XPATH = ".//u"  # this is the option that was selected
+    CHECKBOX_UNIQUE_YES = ".//p[contains(.,'Is this post unique? : yes')]"  # when "yes" was selected
+    CHECKBOX_UNIQUE_NO = ".//p[contains(.,'Is this post unique? : no')]"  # when "no" was selected
+    CHECKBOX_YES_TEXT = "Is this post unique? : yes"
+    CHECKBOX_NO_TEXT = "Is this post unique? : no"
--- pages/create_post_page.py
+++ pages/create_post_page.py
@@ -24,25 +24,40 @@ class CreatePostPage(BasePage):
         assert self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH) == self.constants.SUCCESS_MESSAGE_TEXT, \
             f"Actual: {self.get_element_text(xpath=self.constants.SUCCESS_MESSAGE_XPATH)}"
 
-    def create_post_with_checkbox(self, post):
+    def create_post_with_checkbox_dropdown(self, post):
         """Create post using provided values"""
+        """Post Unique = YES and dropdown choice = ONE PERSON"""
+        # filling the title and body of the post:
         self.fill_field(xpath=self.constants.TITLE_FIELD_XPATH, value=post.title)
         self.fill_field(xpath=self.constants.BODY_FIELD_XPATH, value=post.body)
-
-        self.click(xpath=self.constants.CHECKBOX_INPUT_XPATH)  # added checkbox click
-        self.click(xpath=self.constants.DROPDOWN_LIST_XPATH)  # added dropdown click and one option select
-        self.click(self.constants.VISIBILITY_SELECTION_XPATH.format(option=self.constants.OPTION_ONE_PERSON))
+        # checkbox unique post click-check:
+        self.click(xpath=self.constants.CHECKBOX_UNIQUE_POST_INPUT_XPATH)
+        # dropdown click and select of one option:
+        self.click(xpath=self.constants.DROPDOWN_LIST_XPATH)
+        self.click(self.constants.VISIBILITY_SELECTION_XPATH.format(option=self.constants.OPTION_ONE_PERSON_TEXT))
         self.click(xpath=self.constants.CREATE_POST_BUTTON_XPATH)
 
     @log_decorator
     def verify_checkbox(self):
         """Verify checkbox Unique"""
-
-        assert self.get_element_text(self.constants.CHECKBOX_UNIQUE_YES) == self.constants.CHECKBOX_TEXT, \
+        assert self.get_element_text(self.constants.CHECKBOX_UNIQUE_YES) == self.constants.CHECKBOX_YES_TEXT, \
             f"Actual message: {self.get_element_text(self.constants.CHECKBOX_UNIQUE_YES)}"
 
     @log_decorator
     def verify_dropdown_select(self):
         """Verify DROPDOWN SELECT"""
-        assert self.get_element_text(xpath=self.constants.SELECT_XPATH_GROUP) == self.constants.SELECT_XPATH_TEXT, \
-            f"Actual: {self.get_element_text(xpath=self.constants.SELECT_XPATH_GROUP)}"
+        assert self.get_element_text(xpath=self.constants.SELECTED_VISIBILITY_VALUE_XPATH) == \
+               self.constants.OPTION_ONE_PERSON_TEXT, f"Actual:" \
+                                                      f" {self.get_element_text(xpath=self.constants.SELECTED_VISIBILITY_VALUE_XPATH)}"
+
+    @log_decorator
+    def verify_post_title(self, post):
+        """Verify post title"""
+        assert self.get_element_text(xpath=self.constants.CREATED_TITLE_XPATH) == post.title, \
+            f"Actual: {self.get_element_text(xpath=self.constants.CREATED_TITLE_XPATH)}"
+
+    @log_decorator
+    def verify_post_body(self, post):
+        """Verify body"""
+        assert self.get_element_text(
+            xpath=self.constants.CREATED_BODY_CONTENT_XPATH.format(body=post.body)) == post.body
